07891c142dffdd671e88fe2842ac81f8de44bd4c
sudal <gaesudal@gmail.com>
2022-11-24 12:18:47 +0900

    section3 - 배열과 오브젝트의 성능평가
    section3 - 배열과 오브젝트의 성능평가

---
+++ #3.배열과 오브젝트의 성능평가.js
@@ -0,0 +1,9 @@
+let instructor = {
+    firstName: "Kelly",
+    isInstructor: true,
+    favoriteNumbers: [1,2,3,4]
+}
+
+let names = ["Michael", "Melissa", "Andrea"];
+
+let values = [true, {}, [], 2, "awesome"];
