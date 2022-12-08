f33aeac40a42b6ba954de11450e6408dafe13b91
sudal <gaesudal@gmail.com>
2022-12-08 16:18:08 +0900

    sction5 - 문제 해결 패턴
    sction5 - 문제 해결 패턴

--- #5.문제 해결 패턴.js
+++ #5.문제 해결 패턴.js
@@ -60,3 +60,64 @@ function sumZeroRefactored (arr) {
         }
     }
 }
+
+function maxSubarraySum (arr, num) {
+    if (num > arr.length) {
+        return null;
+    }
+    var max = -Infinity;
+    for (let i = 0; i < arr.length - num + 1; i++) {
+        temp = 0;
+        for (let j = 0; j < num ; j++) {
+            temp += arr[i + j];
+        }
+        if (temp > max) {
+            max = temp;
+        }
+        return max;
+    }
+}
+
+function maxSubarraySumRefactor (arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    for ( let i = num; i < arr. length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSubarraySum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+function search(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            return i;
+        }
+        return -1
+    }
+}
+
+function searchRefactor(arr, val) {
+    let min = 0;
+    let max = arr.length - 1;
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = arr[middle];
+        
+        if (arr[middle] < val) {
+            min = middle + 1;
+        }
+        else if (arr[middle] > val) {
+            max = middle - 1;
+        }
+        else {
+            return middle;
+        }
+    }
+    return -1;
+}
